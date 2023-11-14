18dced364d449351efe3286d6b7074c3238daaa3
carlos-resto <bataraputra567@gmail.com>
2023-11-14 09:46:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Resto Enak</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Resto Enak</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Hot Promo</a></li>
+                <li><a href="#">Makanan</a></li>
+                <li><a href="#">Minuman</a></li>
+                <li><a href="#">Desert</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <section class="menu">
+        <h2>Menu Resto Enak</h2>
+        <div class="menu-item">
+            <img src="img/bakso.jpeg" alt="menu 1">
+            <h3>Bakso Jumbo</h3>
+            <p>
+               terdiri dari 1 bakso kasar dan 1 bakso jumbo.
+            </p>
+            <p class="harga">Harga : rp. 10.000</p>
+            <button class="order-button">Pesan</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/baksooo.jpeg" alt="menu 2">
+            <h3>Bakso Campur</h3>
+            <p>
+                Perpaduan berbagai macam bakso mulai dari bakso campur,urat,kasar dll.
+            </p>
+            <p class="harga">Harga : rp. 12.000</p>
+            <button class="order-button">pesan</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/mie ayam ceker.jpg" alt="menu 3">
+            <h3>Mie Ayam Ceker</h3>
+            <p>
+                Mie ayam dengan tambahan ceker yang enak.
+            </p>
+            <p class="harga">Harga : rp. 14.000</p>
+            <button class="order-button">pesan</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/baksooo.jpeg" alt="menu 4">
+            <h3>Mie Ayam</h3>
+            <p>
+                mie ayam biasa yang terdiri sayur dan ayam yang dicampur dengan bumbu-bumbu yang enak.
+            </p>
+            <p class="harga">Harga : rp. 11.000</p>
+            <button class="order-button">pesan</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/es jeruk.jpeg" alt="menu 5">
+            <h3>es jeruk</h3>
+            <p>
+                buah jeruk yang diperas dan dikasih gula sama es.
+            </p>
+            <p class="harga">Harga : rp. 4.000</p>
+            <button class="order-button">pesan</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/es teh.jpg" alt="menu 6">
+            <h3>Es Teh</h3>
+            <p>
+                es teh biasa yang dibuat dengan teh celup atau teh tubruk.
+            </p>
+            <p class="harga">Harga : rp. 3.000</p>
+            <button class="order-button">pesan</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/es campur.jpg" alt="menu 7">
+            <h3>Es Campur</h3>
+            <p>
+                Perpaduan es yang terdiri dari berbagai isi yaitu mutiara,nanas,jeli dll.
+            </p>
+            <p class="harga">Harga : rp. 8.000</p>
+            <button class="order-button">pesan</button>
+        </div>
+        <div class="menu-item">
+            <img src="img/aqua.jpeg" alt="menu 8">
+            <h3>Aqua</h3>
+            <p>
+                air putih biasa ges bikin kuat.
+            </p>
+            <p class="harga">Harga : rp. 5.000</p>
+            <button class="order-button">pesan</button>
+        </div>
+    </section>
+
+    <section class="contact">
+        <h2>Hubungi Kami</h2>
+        <p>Jika Ada Pertanyaan, saran atau kritik hubungi kami di:</p>
+        <p>Wa 08123635524</p>
+        <p>email : admin@gmail.com</p> <img src="img/suryanto.jpeg">
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Restoran Enak.Hak Cipta di lindungi</p>
+    </footer>
+</body>
+</html>
